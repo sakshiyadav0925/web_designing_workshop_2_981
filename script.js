788c8351aa56adc3790089e61008d45982edd42b
sakshiyadav0925 <yadavsakshi0332@gmail.com>
2026-06-19 20:58:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
-let employees =[] ;
-function addEmployee() {
-    let emp = {
-        name: document.getElementById("name").value,
-        id: document.getElementById("id").value, 
-        salary: Number(document.getElementById("salary").value),
-        dept: document.getElementById("dept").value
-    } ;
-    employees.push(emp) ;
-    alert("Employee Added!") ;
+function calculateResult() {
+    let subjects =Numberlet total=0;
+    for (int i=1;i<+subjects;i++) {
+        let marks =Number(Prompt("Enter marks for subject " +1));
+         total+=marks ;
+    } 
+    let average=total /subjects;
+    let grade;
+    let result;
+    if(average>=90) {
+        grade="a";
+        result="pass" ;
+        else if()
 
-}
-function displayEmployees() {
-    let output = " " ;
- for(let emp of employees) {
-    output += emp.name + "- rupees" +emp.salary +"<br>" ;
 
-}
-document.getElementById("output").innerHTML = output ;
- 
-function filterEmployees() {
-    let result =employees.filter(emp => emp.salary>5000) ;
-    let output=" " ;
 
-}
 
-function totalSalary() {
-    let total =0 ;
-    for(let emp of employees) {
-        total += emp.salary ;
+
     }
-    document.getElementById("output").innerHTML ="Total Salary : $" +total ;
-
-}
-function avgsalary() {
-    let total = 0 ;
-    document.getElementById("output").innerHTML ="Average salary : $" 
+    document .getElementById("output") .innerHTML =
+    "Total Marks: " + total + "<br>" +
+    "Average marks" + average +"<br>" +
+     "Grade :" + grade + "<br>" +
+     "Result :" +result ;
 }
